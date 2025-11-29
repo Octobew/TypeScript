63640963c53c1cb6b8b774e51428522644f34b4a
APISARA ARUNWONG <apisara_ar@cmu.ac.th>
2025-11-29 17:15:12 +0700

    Task 10.1

--- src/for.ts
+++ src/for.ts
@@ -1,10 +1,4 @@
-let names = ['Alice', 'Bob', 'Catherine', 'Eden', 'Johnny']
-for (let i = 0; i < names.length; i++) {
-    const name = names[i]
-    if (name && name.length > 6) {
-        console.log(`สวัสดีคุณ ${name}`)
-    }
-    else{
-        console.log(`Hello ${name}`)
-    }
-    }
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']  
+for (const c of color) {  
+  console.log(c);  
+} 
