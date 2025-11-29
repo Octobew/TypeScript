650e36ab235362fc1b5fbe5129a3ea3d7c714383
APISARA ARUNWONG <apisara_ar@cmu.ac.th>
2025-11-29 16:30:43 +0700

    Task 8

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello, TypeScript with Node.js!");
+let x:number = 9;
+let y:number = 10;
+if (x > y && y > 12) {
+    console.log('ค่ามากจัง');
+} else {
+    console.log(x*y + x/y);
+}
